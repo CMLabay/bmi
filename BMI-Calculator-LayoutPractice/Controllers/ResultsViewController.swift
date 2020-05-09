2b25375a8cad45892d83ec4495e2272787634e60
Christina Labay <Christina.m.labay@gmail.com>
2020-05-09 18:00:57 -0500

    add results controller

---
+++ BMI-Calculator-LayoutPractice/Controllers/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Christina Labay on 5/9/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
